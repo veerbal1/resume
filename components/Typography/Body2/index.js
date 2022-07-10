7d56166dbda862d111f14666bde4d9572af5f81d
veerbal1 <veerbal1singh1@gmail.com>
2022-07-10 18:33:20 +0530

    Added prop to highlight text inside body2 component

--- components/Typography/Body2/index.js
+++ components/Typography/Body2/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from '../../../styles/typography/Body2.module.scss';
 
-const Body2 = ({ children }) => {
-  return <h6 className={styles.body2}>{children}</h6>;
+const Body2 = ({ children, highlight }) => {
+  return <h6 className={`${styles.body2} ${highlight ? styles.highlight:''}`}>{children}</h6>;
 };
 
 export default Body2;
