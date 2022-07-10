477eadcbed26cd8a79491e2d278da16978025ffd
veerbal1 <veerbal1singh1@gmail.com>
2022-07-10 19:19:22 +0530

    Created Intro component

---
+++ UIElements/ContentBody/Intro/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Body2 from '../../../components/Typography/Body2';
+import myInfo from '../../../info/my-info';
+import Name from '../../../components/Typography/Name';
+
+const Intro = () => {
+  return (
+    <>
+      {/*Current Role */}
+      <Body2>{myInfo.main.title['en']}</Body2>
+      {/* Name */}
+      <Name>Veerbal Singh</Name>
+    </>
+  );
+};
+
+export default Intro;
---
+++ info/my-info.js
@@ -0,0 +1,24 @@
+const myInfo = {
+  main: {
+    name: {
+      en: 'Veerbal Singh',
+      de: 'Veerbal Singh',
+      es: 'Veerbal Singh',
+    },
+    title: {
+      en: 'Software Engineer',
+      de: 'Software Engineer',
+      es: 'Software Engineer',
+    },
+  },
+  contact: {
+    email: 'veerbalsingh1@gmail.com',
+    phone: '+49 176 604 604',
+    address: {
+      en: 'Berlin, Germany',
+      de: 'Berlin, Deutschland',
+    },
+  },
+};
+
+export default myInfo;
